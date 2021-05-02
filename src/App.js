a4ab361b54fa0c8964b717d715614fd55a9c9d20
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-05-02 16:33:48 +0200

    e

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MobileNavigation from "./NavBar/MobileNavigation";
+import Navigation from "./NavBar/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+    <MobileNavigation />
+      {/* <Navigation /> */}
+    </div>
+  );
+}
+
+export default App;
